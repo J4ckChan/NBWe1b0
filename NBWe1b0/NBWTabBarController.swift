ddeac8b4259dc78c05e5fcff87e576e9a9bf7c47
JackChan829 <liang.chen829@gmail.com>
2016-01-09 01:40:21 +0800

    add uitabbarcontroller

---
+++ NBWe1b0/NBWTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  NBWTabBarController.swift
+//  NBWe1b0
+//
+//  Created by ChanLiang on 1/9/16.
+//  Copyright © 2016 JackChan. All rights reserved.
+//
+
+import UIKit
+
+class NBWTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
